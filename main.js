d0c9734e6f3713bc8ce2881ed9189309904a1242
Koran Agan <koranagan@gmail.com>
2021-02-16 10:30:50 -0500

    refactor and merge the logic of playing sound with keydown && onclick

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+
+
+
+
 function playRandomPitch(range) {// range is picked in accordance to level difficulty. Choices of different Node lists could unlock different levels. (diatonic, chromatic, number of octaves etc.)
+   
    const random_index = Math.floor(
       Math.random() * Math.floor(range))
     
@@ -9,14 +14,22 @@ function playRandomPitch(range) {// range is picked in accordance to level diffi
     }, 1000)
 }
 
-function playPianoOnKeydown(e) {
+function playPiano(e) {
+   let playedSound;
+   let pianoKey;
    if (e.repeat) return; //stops event 'keydown' from continous fireing
-   const playedSound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   const pianoKey = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   if (e.type == 'keydown') {
+      playedSound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      pianoKey = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   } else {
+      playedSound = document.querySelector(`audio[data-key="${e.path[0].attributes[0].value}"]`)
+      pianoKey = document.querySelector(`.key[data-key="${e.path[0].attributes[0].value}"]`)
+   }
    if (!playedSound) return; //exits function if computer keys without assigned sound pressed
    playedSound.currentTime = 0; 
    playedSound.play();
    pianoKey.classList.add('finger-down');// Transition style for pressed piano key
+   if (!document.querySelector('.ebony-ivory-together.et-mode')) return; // activate ear-trainer mode
    playRandomPitch(15);
 }
 
@@ -30,19 +43,9 @@ function fingerUp(e) {
 
 pianoKeys.forEach(pianoKey => pianoKey.addEventListener('transitionend', fingerUp));
 
-window.addEventListener('keydown', playPianoOnKeydown);
+window.addEventListener('keydown', playPiano);
+window.addEventListener('click', playPiano);
 
-
-//////////// Playing notes with the mouse (trackpad, touchscreen)
-function PlayPianoOnClick(e) {
-   const clickedSound = document.querySelector(`audio[data-key="${e.path[0].attributes[0].value}"]`)
-   clickedSound.play();
-   playRandomPitch(15);
-}
-
-document.querySelectorAll('.key').forEach(item => {
-   item.addEventListener('click', PlayPianoOnClick)
-   })
 
 
 
